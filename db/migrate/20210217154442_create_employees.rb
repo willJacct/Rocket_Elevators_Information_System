feeb3a8e07794da1f43c39edde45426fef289c07
willJacct <wj.eslapps@outlook.com>
2021-02-17 08:56:39 -0800

    changed to migrate employees data

--- db/migrate/20210217154442_create_employees.rb
+++ db/migrate/20210217154442_create_employees.rb
@@ -1,6 +1,11 @@
 class CreateEmployees < ActiveRecord::Migration[5.2]
   def change
     create_table :employees do |t|
+      t.string :last_name, null:false
+      t.string :first_name, null:false
+      t.string :title, null:false
+      t.email :email, null:false
+      //t.belongs_to :typ_
 
       t.timestamps
     end
