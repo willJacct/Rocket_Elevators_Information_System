2e25b2e3daa52f190818cd8701902d76cf73a41f
“[Don <[don_nguyen2001@yahoo.com]>
2021-02-17 09:49:05 -0800

    Links work

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  root 'pages#residential'
-  root 'pages#commercial'
-  root 'pages#quote'
+
+
+  get "residential" => "pages#residential"
+  
+  get "commercial" => "pages#commercial"
+
   get "quote" => "pages#quote"
-  #get "residential" => "pages#residential"
+
+  get "index"  => "pages#index"
+
+  root 'pages#index'
+
+  
 
 end
