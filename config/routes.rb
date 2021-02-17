c2735f4eef8f4b7af304487e1658439135d3a6de
tatiana ramirez <tatiana.w@hotmail.com>
2021-02-17 17:25:31 -0500

    revert 023b4 9c4a8 e4873

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -13,11 +12,6 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
-  devise_for :users, :controllers => {:passwords => "users/passwords", :confirmatios=> "users/confirmations"}
-  devise_scope :users do
-    get 'signup', to: 'users/registrations#new'
-    get 'signin', to: 'users/sessions#new'
-    delete 'signout', to: 'users/sessions#destroy'
-  end
+  
 
 end
