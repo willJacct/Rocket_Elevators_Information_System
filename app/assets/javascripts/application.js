53631cff846bb053d6ef2e5a28278756ef9abd8d
Adetula <olaadetula@gmail.com>
2021-02-16 20:07:06 -0500

    commit1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,18 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery
+//= require jquery-ui
+//= require bootstrap-sprockets
+
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+
+
+
