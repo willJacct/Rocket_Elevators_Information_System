783f185f0a1e0fd774998bd621399d1859090314
willJacct <77292560+willJacct@users.noreply.github.com>
2021-02-19 19:47:09 -0800

    Update quotes.rb

--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -1,13 +1,40 @@
 ActiveAdmin.register Quote do
-   permit_params :email, :password, :password_confirmation
+   # permit_params :email, :password, :password_confirmation
    index do
      selectable_column
      id_column
+     column :name
+     column :email
+     column :phone
      column :building_type
+     column :number_apartments_residential
+     column :number_floors_residential
+     column :number_basements_residential
+     column :number_floors
+     column :number_apartments
+     column :number_basements
+     column :number_companies
+     column :number_floors_commercial
+     column :number_parking_commercial
+     column :number_elevators
+     column :number_corporate
+     column :number_floors_corporate
+     column :number_basements_corporate
+     column :number_parking_corporate
+     column :number_occupants_corporate
+     column :number_of_corporations
+     column :number_floors_hydrid
+     column :number_basements_hybrid
+     column :number_parking_hybrid
+     column :number_occupants_hybrid
+     column :number_hours_act
      column :service_level
+     column :elevators_required
+     column :elevator_unit_price
+     column :elevator_total_price
+     column :installation_fees
      column :final_price
      actions
    end
  
  end
- 
